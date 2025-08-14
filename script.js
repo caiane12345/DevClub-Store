cd472568132b0842cebc278b27b85811527ca6ff
Caiane <caiane.i.santos@ba.estudante.senai.br>
2025-08-14 13:33:23 -0300

    sexto commit

--- script.js
+++ script.js
@@ -27,14 +27,17 @@ function update(direction) {
         active = active -1
     }
 
-    else if(direction < 0){
-        active = total -1
-    }
-
     items[active].classList.add('active')
     dots[active].classList.add('active')
+
+    numberIndicator.textContent = String(active + 1).padStart(2,'0')
     
 }
+
+clearInterval(timer)
+timer = setInterval(() => {
+    update(1)
+}, 5000);
 
 prevButton.addEventListener('click', () => {
    update(-1)
